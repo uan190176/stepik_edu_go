2ed31461c15ae874d29947e663331aac1758b41f
Александр Усольцев <uan-macmini@Mac-mini-Aleksandr.local>
2022-09-06 07:57:38 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module stepik_edu
+
+go 1.19
---
+++ prog_1.3.1.go
@@ -0,0 +1,14 @@
+/*
+Напишите программу, которая выводит "I like Go!"
+Sample Input:
+
+Sample Output:
+I like Go!
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!")
+}
